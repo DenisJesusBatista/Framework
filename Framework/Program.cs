3a340715db7e011adecaaefb50abe14fc2499440
Denis Batista <denis.si2011@gmail.com>
2021-01-19 19:48:35 -0300

    Add project files.

---
+++ Framework/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibraryFrameworkSample;
+
+namespace Framework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Assembly
+            Console.WriteLine("Teste");
+            Console.ReadKey();
+
+            Livros livro = new Livros();
+            
+        }
+    }
+}
